af6933cd8f729c0ff473c9e42893d3729b5cc533
Rafi <mcintoshhd@Mac.gateway>
2024-09-22 10:27:36 +1000

    slicing sign-in page and change a tag to Link provided by react-router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import ManagerHome from "../pages/ManagerHome";
+import SignIn from "../pages/SignIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ManagerHome />,
+  },
+  {
+    path: "/manager/sign-in",
+    element: <SignIn />,
   },
 ]);
 
